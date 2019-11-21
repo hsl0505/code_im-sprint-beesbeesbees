ba1be5c993636d510518d50ca8fbfa7615c58a47
hyunsung <hslgood@gmail.com>
2019-11-21 12:01:23 +0900

    es6 complete by hs

--- src/es6/Grub.js
+++ src/es6/Grub.js
@@ -1,5 +1,17 @@
 class Grub {
   // TODO..
+  constructor() {
+    this.age = 0;
+    this.color = "pink"
+    this.food = "jelly"
+  }
+  
+  // 애로우 펑션으로 수정 필요!!
+
+  eat () {
+    return `Mmmmmmmmm ${this.food}`
+  }
+  
 }
 
 module.exports = Grub;
